3265bc27bcfcd6e2a398dedb227e0c9ff7c41240
sreejith980 <117563770+sreejith980@users.noreply.github.com>
2023-06-16 21:59:31 +0530

    3rd assignment

---
+++ 3rd assignment/api-ser.py
@@ -0,0 +1,17 @@
+from flask import Flask,jsonify
+from src.data_reader import show_the_write
+
+app=Flask(__name__)
+
+@app.route('/api/printHello')
+def hello():
+    data={'value':'Hello World'}
+    return jsonify(data)
+
+@app.route('/api/modifyRecepie')
+def modrec():
+    data=show_the_write()
+    return jsonify(data)   
+
+if __name__=='__main__':
+    app.run(debug=True)
